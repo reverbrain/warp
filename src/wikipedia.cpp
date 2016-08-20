639d7a64c484208c1f4658757516502c8a2bfe41
Evgeniy Polyakov <zbr@ioremap.net>
2016-08-20 21:52:49 +0300

    wikipedia: xml parser has been moved to ribosome

--- src/wikipedia.cpp
+++ src/wikipedia.cpp
@@ -19,149 +19,13 @@
 #include <ribosome/lstring.hpp>
 #include <ribosome/split.hpp>
 #include <ribosome/timer.hpp>
+#include <ribosome/xml.hpp>
 
 using namespace ioremap;
 
-typedef std::map<std::string, std::vector<std::string>> attributes_map_t;
-struct element {
-	std::vector<XML_Char> chars;
-	attributes_map_t attrs;
-	std::string name;
-	int thread_num;
-};
-
-class expat_parser
-{
+class wiki_parser : public ribosome::xml_parser {
 public:
-	expat_parser(int n = 1) {
-		m_parser = XML_ParserCreate(NULL);
-		if (!m_parser) {
-			ribosome::throw_error(-ENOMEM, "could not create XML parser");
-		}
-
-		XML_SetUserData(m_parser, this);
-		XML_SetElementHandler(m_parser, expat_start, expat_end);
-		XML_SetCharacterDataHandler(m_parser, expat_characters);
-
-		m_current.reset(new element);
-		for (int i = 0; i < n; ++i) {
-			m_pool.emplace_back(std::thread(std::bind(&expat_parser::callback, this, i)));
-		}
-	}
-
-	~expat_parser() {
-		m_need_exit = true;
-		m_pool_wait.notify_all();
-		for (auto &t: m_pool) {
-			t.join();
-		}
-
-		XML_ParserFree(m_parser);
-	}
-
-	ribosome::error_info feed_data(const char *data, size_t size) {
-		int err = XML_Parse(m_parser, data, size, size == 0);
-		if (err == XML_STATUS_ERROR) {
-			return ribosome::create_error(err, "could not parse chunk: size: %ld, last: %d, line: %ld, error: %s",
-					size, size == 0,
-					XML_GetCurrentLineNumber(m_parser),
-					XML_ErrorString(XML_GetErrorCode(m_parser)));
-		}
-
-		return ribosome::error_info();
-	}
-
-private:
-	XML_Parser m_parser;
-	std::unique_ptr<element> m_current;
-
-	bool m_need_exit = false;
-	std::mutex m_lock;
-	std::deque<std::unique_ptr<element>> m_elements;
-	std::condition_variable m_pool_wait, m_parser_wait;
-	std::vector<std::thread> m_pool;
-
-	static void expat_start(void *data, const char *el, const char **attr) {
-		expat_parser *p = (expat_parser *)data;
-
-		p->m_current->name.assign(el);
-
-		for (size_t i = 0; attr[i]; i += 2) {
-			std::string aname(attr[i]);
-			std::string aval(attr[i+1]);
-
-			auto it = p->m_current->attrs.find(aname);
-			if (it == p->m_current->attrs.end()) {
-				p->m_current->attrs[aname] = std::vector<std::string>({aval});
-			} else {
-				it->second.emplace_back(aval);
-			}
-		}
-	}
-	static void expat_end(void *data, const char *el) {
-		expat_parser *p = (expat_parser *)data;
-
-		(void)el;
-
-		p->element_is_ready();
-	}
-
-	static void expat_characters(void *data, const XML_Char *s, int len) {
-		expat_parser *p = (expat_parser *)data;
-
-		p->m_current->chars.insert(p->m_current->chars.end(), s, s + len);
-	}
-
-	void element_is_ready() {
-		std::unique_lock<std::mutex> guard(m_lock);
-		m_elements.emplace_back(std::move(m_current));
-		m_current.reset(new element);
-
-		if (m_elements.size() > m_pool.size() * 2) {
-			m_parser_wait.wait(guard, [&] {return m_elements.size() < m_pool.size();});
-		}
-
-		guard.unlock();
-		m_pool_wait.notify_one();
-	}
-
-	void callback(int num) {
-		while (!m_need_exit) {
-			std::unique_lock<std::mutex> guard(m_lock);
-			m_pool_wait.wait_for(guard, std::chrono::milliseconds(100), [&] {return !m_elements.empty();});
-
-			while (!m_elements.empty()) {
-				std::unique_ptr<element> elm = std::move(m_elements.front());
-				m_elements.pop_front();
-				guard.unlock();
-				m_parser_wait.notify_one();
-
-				elm->thread_num = num;
-				on_element(*elm);
-
-				guard.lock();
-			}
-		}
-	}
-
-protected:
-	virtual void on_element(const element &elm) {
-		for (auto &p: elm.attrs) {
-			std::cout << elm.name << "::" << p.first << "=";
-			for (auto &aval: p.second) {
-				std::cout << aval << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		ribosome::lstring ls = ribosome::lconvert::from_utf8(elm.chars.data(), elm.chars.size());
-		std::cout << "chars (" << ls.size() << ") : " << ribosome::lconvert::to_string(ls) << std::endl;
-	}
-};
-
-class wiki_parser : public expat_parser {
-public:
-	wiki_parser(int n, const std::string &alphabet) : expat_parser(n), m_alphabet(alphabet) {
+	wiki_parser(int n, const std::string &alphabet) : ribosome::xml_parser(n), m_alphabet(alphabet) {
 		m_model.resize(n);
 	}
 	~wiki_parser() {
@@ -202,7 +66,7 @@ public:
 		return ribosome::error_info();
 	}
 
-	void on_element(const element &elm) override {
+	void on_element(const ribosome::element &elm) override {
 		if (elm.thread_num >= (int)m_model.size()) {
 			ribosome::throw_error(-E2BIG, "invalid thread number: %d, must be less than number of workers: %ld",
 					elm.thread_num, m_model.size());
